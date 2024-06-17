d9e4de609df3e33545c677a97ab9e6734fbe5cc2
Anna Ostanina <a.ostanina@sportradar.com>
2024-06-17 16:03:12 +0200

    feat: add cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import auth, calculator, users
 
 # Create a FastAPI instance
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 # Include the authentication, calculator, and user routers
 app.include_router(auth.router)
